2a9d0f88b0627bd1c1598b65477fe01b82373c39
Shubham Garg <shubhamjnv1999@gmail.com>
2024-01-07 20:19:31 +0530

    Added Working Alarm and Room database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
-    namespace = "com.example.alarmdiary"
+    namespace ="com.example.alarmdiary"
     compileSdk = 34
 
     defaultConfig {
@@ -26,27 +27,31 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
         jvmTarget = "17"
     }
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation ("androidx.room:room-ktx:2.6.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation ("com.android.support:appcompat-v7:34.0.0")
     implementation("com.google.android.material:material:1.11.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation ("androidx.activity:activity-compose:1.8.2")
-    implementation ("androidx.compose.ui:ui:1.5.4")
-    implementation ("androidx.compose.material:material:1.5.4")
+    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.compose.ui:ui:1.5.4")
+    implementation("androidx.compose.material:material:1.5.4")
     implementation("androidx.compose.material3:material3:1.1.2")
     implementation("androidx.compose.material3:material3-window-size-class:1.1.2")
 }
--- app/src/main/java/com/example/alarmdiary/AlarmFragment.kt
+++ app/src/main/java/com/example/alarmdiary/AlarmFragment.kt
@@ -1,5 +1,6 @@
 package com.example.alarmdiary
 
+import com.example.alarmdiary.data.*
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +11,14 @@ import android.widget.Spinner
 import android.widget.TimePicker
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import kotlinx.coroutines.withContext
 import java.util.*
 
 class AlarmFragment : Fragment() {
@@ -26,7 +35,6 @@ class AlarmFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_alarm, container, false)
     }
 
@@ -39,7 +47,6 @@ class AlarmFragment : Fragment() {
         spinnerFrequency = view.findViewById(R.id.spinnerFrequency)
         btnSetAlarm = view.findViewById(R.id.btnSetAlarm)
 
-        // Set initial highlighting
         highlightButton(btnStartTime)
 
         btnStartTime.setOnClickListener {
@@ -86,8 +93,6 @@ class AlarmFragment : Fragment() {
     }
 
     private fun updatePickerTime() {
-        // Implement logic to update TimePicker based on the selected button and any other requirements
-        // For example, you can set default values if nothing is selected
         val calendar = Calendar.getInstance()
         val defaultHour = if (isStartTimeSelected) 5 else 22
         val defaultMinute = 0
@@ -96,8 +101,58 @@ class AlarmFragment : Fragment() {
     }
 
     private fun setAlarm() {
-        // Implement logic to set alarms based on the selected start time, end time, and frequency
-        // You can use the values from simpleTimePicker and spinnerFrequency to schedule alarms
-        Toast.makeText(requireContext(), "Alarm set!", Toast.LENGTH_SHORT).show()
+        val startTimeHour = simpleTimePicker.hour
+        val startTimeMinute = simpleTimePicker.minute
+        val endTimeHour = startTimeHour + 1
+        val endTimeMinute = startTimeMinute
+
+        //val frequencyInHours = spinnerFrequency.selectedItem.toString().toInt()
+        val frequencyInHours = 1
+
+        val alarm = Alarm(
+            startTimeHour = startTimeHour,
+            startTimeMinute = startTimeMinute,
+            endTimeHour = endTimeHour,
+            endTimeMinute = endTimeMinute,
+            frequency = "$frequencyInHours hours"
+        )
+
+        GlobalScope.launch(Dispatchers.IO) {
+            val alarmId = MyApp.getInstance().database.alarmDao().insert(alarm)
+            val calendar = Calendar.getInstance().apply {
+                set(Calendar.HOUR_OF_DAY, startTimeHour)
+                set(Calendar.MINUTE, startTimeMinute)
+                set(Calendar.SECOND, 0)
+            }
+
+            val scheduledTime = scheduleAlarm(alarmId, calendar.timeInMillis)
+            withContext(Dispatchers.Main) {
+                Toast.makeText(requireContext(), "Alarm set for $calendar", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    suspend fun scheduleAlarm(alarmId: Long, startTimeInMillis: Long):Long {
+        val alarmManager = requireContext().getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(requireContext(), MyAlarmReceiver::class.java).apply {
+            action = "YOUR_ALARM_ACTION" // Set a unique action string
+            putExtra("ALARM_ID", alarmId)
+        }
+
+        val pendingIntent = PendingIntent.getBroadcast(
+            requireContext(),
+            alarmId.toInt(),
+            intent,
+            PendingIntent.FLAG_IMMUTABLE // Use FLAG_IMMUTABLE instead of FLAG_UPDATE_CURRENT from 31
+        )
+
+        // Replace AlarmManager.INTERVAL_HOUR with your desired frequency
+        alarmManager.setRepeating(
+            AlarmManager.RTC_WAKEUP,
+            startTimeInMillis,
+            AlarmManager.INTERVAL_HOUR,
+            pendingIntent
+        )
+        return alarmId
     }
 }
